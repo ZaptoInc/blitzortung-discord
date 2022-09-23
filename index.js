f1abf611572820c9f7ddf387543330a751867480
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2022-09-23 02:06:32 +0200

    added support dm bot

--- index.js
+++ index.js
@@ -1,31 +1,105 @@
-var websocket = require('ws');
+const websocket = require('ws');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+
 const config = require('config');
+
+const client = new Client({ intents: [] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    NewWebSocket();
+    setInterval(SendPrivateMessage, 60000);
+});
+client.login(config.discordToken);
 
 var latestWarn = 0
 var RecentImpacts = []
 
 function DecodedDataManager(data) {
     var impactDistance = calcCrow(data.lat, data.lon, config.position.lat, config.position.lon)
+
+    if (impactDistance < config.position.watchDistance) {
+        RecentImpacts.push(data)
+    }
+
+    if (impactDistance < config.position.instantWarningDistance) {
+        if (latestWarn + 60000 < Date.now()) {
+            SendPrivateMessage(true, data)
+        }
+    }
+
+
+
+
+
     console.log(impactDistance, 'km')
+}
+
+async function SendPrivateMessage(instant, data) {
+
+    if (instant || RecentImpacts.length) {
+        const user = await client.users.fetch(config.discordId).catch(() => null);
+
+        if (!user) return console.log('User not found :(')
+
+        latestWarn = Date.now()
+
+        var embeds = []
+
+        if (instant) {
+            var impactDistance = calcCrow(data.lat, data.lon, config.position.lat, config.position.lon)
+            var instantEmbed = new EmbedBuilder()
+            .setColor(0xFF0000)
+            .setTitle('Instant Warning!')
+            .setDescription('A thunderbold has hit ' + impactDistance + 'km away <t:' + (data.time / 1000000000) + ':R> ')
+            embeds.push(instantEmbed)
+        }
+
+        if (RecentImpacts.length) {
+            var closest = {}
+            var closestDistance = 100000
+            RecentImpacts.forEach(hit => {
+                var impactDistance = calcCrow(hit.lat, hit.lon, config.position.lat, config.position.lon)
+                if (impactDistance < closestDistance) {
+                    closest = hit
+                }
+            });
+            var closestImpactDistance = calcCrow(closest.lat, closest.lon, config.position.lat, config.position.lon)
+            var recentEmbed = new EmbedBuilder()
+            .setColor(0xFF8000)
+            .setTitle('Closeby Warning!')
+            .setDescription(RecentImpacts.length + ' thunderbold has hit with the closest' + closestImpactDistance + 'km away <t:' + (closest.time / 1000000000) + ':R> ')
+            embeds.push(recentEmbed)
+        }
+
+        if (embeds.length) {
+            await user.send({ embeds: [embeds] }).catch(() => {
+                console.log("User has DMs closed or has no mutual servers with the bot :(");
+            });
+        }
+
+    }
+
+
 }
 
 var wsServerId = 1
 var ws;
-function NewWebSocket(){
+function NewWebSocket() {
     try {
-        var wsServer = 'wss://ws' + wsServerId  + '.blitzortung.org/'
+        var wsServer = 'wss://ws' + wsServerId + '.blitzortung.org/'
         console.log('connecting to server %s', wsServerId)
         ws = new websocket.WebSocket(wsServer);
-        ws.on('open', function() {
+        ws.on('open', function () {
             console.log('socket connected')
             ws.send('{"a":767}');
         });
-        ws.on('error', function() {
+        ws.on('error', function () {
             console.log('socket errored');
             NewWebSocketServer()
             setTimeout(NewWebSocket, 1000);
         });
-        ws.on('close', function() {
+        ws.on('close', function () {
             console.log('socket disconnected')
             setTimeout(NewWebSocket, 1000);
         });
@@ -44,15 +118,13 @@ function NewWebSocket(){
 
 };
 
-function NewWebSocketServer(){
+function NewWebSocketServer() {
     if (wsServerId = 8) {
         wsServerId = 1
     } else {
         wsServerId += 1
     }
 }
-
-NewWebSocket();
 
 // Imported from https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
 // --->
